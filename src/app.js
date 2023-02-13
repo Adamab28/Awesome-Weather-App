f9950f43e453c4ca459627c3104f2cae4bfa8d02
Adamab28 <124196478+Adamab28@users.noreply.github.com>
2023-02-13 18:55:27 +0000

    added axios link and ids

--- src/app.js
+++ src/app.js
@@ -1,10 +1,53 @@
- function displayTemperature(response) {
-    console.log (response.data);
-    let temperatureElement = docunement.querySlecetor("#temperature");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+function formateDate (timestamp)
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySlecetor("#temperature");
+  let cityElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formateDate(response.data.time * 1000);
+}
+
+function search(city) {
+  let apiKey = "t33fdeba1d36f24d4ao348fa86c60bb5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
